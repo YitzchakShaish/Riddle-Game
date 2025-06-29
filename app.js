b7118d6457651d5856f40e3fa1069768898e0592
yitzchak shaish <y350380350@gmail.com>
2025-06-29 20:08:06 +0300

    Enhance player initialization and add game start question for improved gameplay flow

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ import AllRiddles from './riddles/AllRiddles.js';
 import P from './classes/Player.js';
 import { question } from 'readline-sync';
 import chalk from 'chalk';
+import players from './playeres/listPlayers.js'
 
+ function ShowPuzzleAndCalculateTime(fn,player){
+        const start =Date.now();
+        fn()();
+        const end = Date.now();
+        player.recordTime(start,end)
+ }
 
+    
 
 // import r1 from './riddles/r1.js';
 // import r2 from './riddles/r2.js';
@@ -13,13 +21,17 @@ import chalk from 'chalk';
 console.log(chalk.green(`Welcome to the Riddle Game! `))
 const name = question(`What is your name? `)
 console.log(`Hello, ${name}!`);
+const gameStarts = question(`When the game starts, ask: `)
 
-const player = new P.Player(name);
+
+const player = new P.Player(name, gameStarts);
 
 AllRiddles.forEach((riddle) => {
-  const start =Date.now();
-  riddle.ask();
-  const end = Date.now();
-  player.recordTime(start,end);
+  // ShowPuzzleAndCalculateTime(()=>riddle.ask, player)
+  if( riddle.riddeleStarts == player.gameStarts){
+        const start =Date.now();
+        riddle.ask();
+        const end = Date.now();
+        player.recordTime(start,end);}
 });
 player.showStats();
